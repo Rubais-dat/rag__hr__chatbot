74c139fefb602a3a7bc0d2e3439769d665db90ae
Rubais KT <rubaiskt@gmail.com>
2026-05-25 14:02:38 +0530

    edited code

--- src/retriever.py
+++ src/retriever.py
@@ -4,7 +4,7 @@ import faiss
 import numpy as np
 
 class Retriever:
-    def __init__(self, embeddings_file= r"C:\Users\rubai_s8od3y5\rag__hr__chatbot\data\HR_policy_embeddings_local.csv"):
+    def __init__(self, embeddings_file= "data\HR_policy_embeddings_local.csv"):
         self.embeddings_file = embeddings_file
         self.chunks = []
         self.vectors = []
